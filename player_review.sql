59d3e3460dfb4145c7eb46a6b457cdd7fddc2782
pkaram <karamerosp@gmail.com>
2020-09-24 19:02:58 +0200

    modify readme

--- player_review.sql
+++ player_review.sql
@@ -7,10 +7,7 @@
     ict_index,
 	minutes,
 	now_cost/10 as cost,
-    case when player_type.singular_name='Defender' then 1 else 0 end as defender,
-    case when player_type.singular_name='Midfielder' then 1 else 0 end as midfielder,
-    case when player_type.singular_name='Goalkeeper' then 1 else 0 end as goalkeeper,
-    case when player_type.singular_name='Forward' then 1 else 0 end as forward
+    player_type.singular_name as position
     from players
     join player_type on player_type.id=players.element_type
 	join teams on teams.code=players.team_code
