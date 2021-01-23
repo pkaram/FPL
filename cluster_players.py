9e9d99c6e60a6a849d2ef01e01d3f296d0874504
pkaram <karamerosp@gmail.com>
2021-01-23 20:43:43 +0100

    modify script and add airflow dag

--- cluster_players.py
+++ cluster_players.py
@@ -1,70 +1,59 @@
 #cluster players based on performance metrics
-
 import sqlite3
 from sklearn.cluster import KMeans
 import pandas as pd
-import matplotlib.pyplot as plt
 
-#connect to the database
-cnx = sqlite3.connect('fpl_data.db')
+def main():
+    #connect to the database
+    cnx = sqlite3.connect('fpl_data.db')
 
-#load most recent info on players
-players=pd.read_sql("""
-with teams_subset as (
-select distinct code,name as team_name from teams
-)
+    #load most recent info on players
+    players=pd.read_sql("""
+    with teams_subset as (
+    select distinct code,name as team_name from teams
+    )
+    
+    select 
+    players.*,
+    player_type.singular_name,
+    teams_subset.team_name as team_name
+    from players
+    join player_type on players.element_type=player_type.id
+    join teams_subset on teams_subset.code=players.team_code
+    where players.date_loaded in (select max(date_loaded) as max_date from players)  
+    and player_type.singular_name !='Goalkeeper'              
+    """, cnx)
+    cnx.close()
 
-select 
-players.*,
-player_type.singular_name,
-teams_subset.team_name as team_name
-from players
-join player_type on players.element_type=player_type.id
-join teams_subset on teams_subset.code=players.team_code
-where players.date_loaded in (select max(date_loaded) as max_date from players)  
-and player_type.singular_name !='Goalkeeper'              
-""", cnx)
+    #drop Goalkeepers out of clustering
+    #players_clustering=players[players['singular_name']!='Goalkeeper']
+    #drop out players that have not played (this part can be modified as minutes represent total minutes played)
+    players_clustering=players[players['minutes']!=0]
+    players_clustering=players_clustering.reset_index(drop=True)
 
-#drop Goalkeepers out of clustering
-#players_clustering=players[players['singular_name']!='Goalkeeper']
-#drop out players that have not played (this part can be modified as minutes represent total minutes played)
-players_clustering=players[players['minutes']!=0]
-players_clustering=players_clustering.reset_index(drop=True)
+    #change data structure for clustering
+    players_clustering['influence']=[float(s) for s in players_clustering['influence']]
+    players_clustering['creativity']=[float(s) for s in players_clustering['creativity']]
+    players_clustering['threat']=[float(s) for s in players_clustering['threat']]
+    players_clustering['points_per_game']=[float(s) for s in players_clustering['points_per_game']]
 
-#change data structure for clustering
-players_clustering['influence']=[float(s) for s in players_clustering['influence']]
-players_clustering['creativity']=[float(s) for s in players_clustering['creativity']]
-players_clustering['threat']=[float(s) for s in players_clustering['threat']]
-players_clustering['points_per_game']=[float(s) for s in players_clustering['points_per_game']]
+    #select data to perform kmeans clustering on
+    data_kmeans=players_clustering[['influence','creativity','threat','clean_sheets','points_per_game','minutes']]
 
-#select data to perform kmeans clustering on
-data_kmeans=players_clustering[['influence','creativity','threat','clean_sheets','points_per_game','minutes']]
+    #as an example 4 clusters will be used
+    kmeans=KMeans(n_clusters=4).fit(data_kmeans)
+    #add labels of clusters on the dataframe
+    players_clustering['cluster_label']=kmeans.labels_
+    players_clustering['now_cost']=players_clustering['now_cost'].astype(float)
+    players_clustering['now_cost']=players_clustering['now_cost']/10
+    players_clustering['ict_index']=players_clustering['ict_index'].astype(float)
 
-#define how many clusters make sense
-Sum_of_squared_distances = []
-K = range(1,10)
-for k in K:
-    km = KMeans(n_clusters=k)
-    km = km.fit(data_kmeans)
-    Sum_of_squared_distances.append(km.inertia_)
+    players_clustering=players_clustering[['singular_name','team_name','second_name','now_cost','points_per_game','minutes','cluster_label','ict_index']]
 
-plt.plot(K, Sum_of_squared_distances, 'bx-')
-plt.xlabel('k')
-plt.ylabel('Sum_of_squared_distances')
-plt.title('Elbow Method For Optimal k')
-plt.show()
+    players_clustering=players_clustering.sort_values(by='ict_index',ascending=False)
+    players_clustering=players_clustering.reset_index(drop=True)
+    #write dataframe as table to the database
+    players_clustering.to_sql(name='cluster_results', con=cnx,index=False,if_exists='replace')
 
-#as an example 4 clusters will be used
-kmeans=KMeans(n_clusters=4).fit(data_kmeans)
-#add labels of clusters on the dataframe
-players_clustering['cluster_label']=kmeans.labels_
-players_clustering['now_cost']=players_clustering['now_cost'].astype(float)
-players_clustering['now_cost']=players_clustering['now_cost']/10
-players_clustering['ict_index']=players_clustering['ict_index'].astype(float)
-
-players_clustering=players_clustering[['singular_name','team_name','second_name','now_cost','points_per_game','minutes','cluster_label','ict_index']]
-
-players_clustering=players_clustering.sort_values(by='ict_index',ascending=False)
-players_clustering=players_clustering.reset_index(drop=True)
-#write dataframe as table to the database
-players_clustering.to_sql(name='cluster_results', con=cnx,index=False,if_exists='replace')
+if __name__ == '__main__':
+    main()
